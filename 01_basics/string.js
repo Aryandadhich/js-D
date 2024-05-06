7bcb74162a7dd9a922f913f56615a9c72d8d2604
aryan dadhich <66268067+Aryandadhich@users.noreply.github.com>
2024-05-06 12:43:51 +0000

    string

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -13,4 +13,19 @@ console.log(gameName[0]);
 console.log(gameName.__proto__);
 
 console.log(gameName.toUpperCase());
-console.log(gameName.cha)
+console.log(gameName.charAt('2'))
+console.log(gameName.indexOf('r'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(1,3)
+console.log(anotherString);
+
+const newStringone = "  aryan  "
+console.log(newStringone.trim())
+
+const url = "https://aryan.com//aryan%20dadhich"
+console.log(url.replace('%20','-'))
+
+console.log(url.split('%20'));
