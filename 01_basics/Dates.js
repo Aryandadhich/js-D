c2daf06a10bea47c590b73dcf8de6b0894667788
aryan dadhich <66268067+Aryandadhich@users.noreply.github.com>
2024-05-07 10:13:37 +0000

    ###

--- 01_basics/Dates.js
+++ 01_basics/Dates.js
@@ -1,2 +1,18 @@
 let myDate = new Date()
 console.log(myDate);
+
+//console.log(myDate.toLocaleString());
+
+let myCreateDate = new  Date(2023,0,23)
+console.log(myCreateDate.toDateString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp); //output in milisecond in both
+//console.log(myCreateDate.getTime());
+//console.log(Math.floor(Date.now()/1000));
+
+let newdate = new Date()
+console.log(newdate);
+console.log(newdate.getMonth());
+console.log(newdate.getDay());
+
