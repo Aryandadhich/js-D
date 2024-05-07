24247054b3105e19d41819d7c23f06042f97dd13
aryan dadhich <66268067+Aryandadhich@users.noreply.github.com>
2024-05-07 10:35:57 +0000

    array

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -1,1 +1,26 @@
-//array
+const myArr = [10,20,30,40,50]
+
+//console.log(myArr[0]);
+
+//Array methods
+
+// myArr.push(6)
+// myArr.pop()// delete by default last value
+// console.log(myArr);
+
+// myArr.unshift(9) 
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(20));
+
+const newArr = myArr.join(); // convert in string and give comma seprated values 
+//console.log(newArr);
+//console.log(myArr);
+
+//slice , splice
+
+console.log("A" , myArr);
+const myn1 = myArr.slice(1,3)
+console.log(myn1)
+console.log("B" , myArr);
