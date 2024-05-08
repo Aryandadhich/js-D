65cd0e727b8d7b8ae7765f4e1e76a900cc543ec8
aryan dadhich <66268067+Aryandadhich@users.noreply.github.com>
2024-05-08 12:42:43 +0000

    array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -9,4 +9,18 @@ const dc_hero = ["superman","baburao","ramu"]
 
 const all_new_hero = [... marvel_heros, ...dc_hero]
 
-console.log(all_new_hero);
+//console.log(all_new_hero);
+
+const anotherarray = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const real_anotherarray = anotherarray.flat(Infinity)
+//console.log(real_anotherarray);
+
+console.log(Array.isArray("Aryan"));
+console.log(Array.from("Aryan"));
+console.log(Array.isArray({name:"Aryan"})); // give emptyy array becaue we havbe to keys or value 
+
+let score1 = 100
+let score2 = 200
+let score3 =  300
+
+console.log(Array.of(score1,score2,score3))
