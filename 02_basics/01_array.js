02347fafeca19a7d0915df66ffdb547f7ae554ce
aryan dadhich <66268067+Aryandadhich@users.noreply.github.com>
2024-05-08 07:43:17 +0000

    array.

---
+++ 02_basics/01_array.js
@@ -0,0 +1,8 @@
+const marvel_heros =["thor",'iron_man','spidy']
+const dc_hero = ["superman","baburao","ramu"]
+
+// marvel_heros.push(dc_hero)
+// console.log(marvel_heros[3][0]);
+
+// const all_hero = marvel_heros.concat(dc_hero)
+// console.log(all_hero);
