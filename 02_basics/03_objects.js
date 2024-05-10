cf6bc0f95e79050c8c4b003068b60f725476764a
aryan dadhich <66268067+Aryandadhich@users.noreply.github.com>
2024-05-10 10:09:40 +0000

    object

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,37 @@
+//singleton  
+
+//object literals
+
+const mysyn = Symbol("key1")
+const Jsuser = {
+    name: "Aryan",
+    "full name" : "Aryan dadheech",
+    age : 18,
+    [mysyn] : "mykey1", //syumbol syntex
+    location : "Udaipur",
+    email : "aryan@gmail.com",
+    isLoggedIn : false,
+    lastLoginDays : ["Monday" , " Tuesday"]
+}
+
+//console.log(Jsuser.email)
+//console.log(Jsuser["full name"])
+//console.log((Jsuser[mysyn]))
+
+//Jsuser.email = "aryan1234@gmail.com"
+//console.log(Jsuser.email)
+//Object.freeze(Jsuser) // freezing the value here
+//Jsuser.email = "artyu@gmail.com"
+//console.log(Jsuser);
+
+Jsuser.greetings = function(){
+    console.log("Hello Js user");
+
+}
+
+Jsuser.greetingtwo = function(){
+    console.log(`Hello Js user, ${this.name}`);
+
+}
+console.log(Jsuser.greetings);
+console.log(Jsuser.greetingtwo());
