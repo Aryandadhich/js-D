e55c955bba97520251cc1584d5d5ab1c925ddfbf
aryan dadhich <66268067+Aryandadhich@users.noreply.github.com>
2024-05-16 09:58:30 +0000

    object

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -24,4 +24,20 @@ const obj2 = {2: "a", 4: "b"}
 //.. so we use insted of this 
 const obj3 = Object.assign({},obj1, obj2)
 
-console.log(obj3)
+//console.log(obj3)
+
+const course = {
+    coursename : "js in hindi ",
+    price : "999",
+    courseinstructor : "hitesh"
+}
+
+//course.courseinstructor nornal method to call the value
+//.. if i have to call courseinstructor value 3 times then i have to call 3 times so
+//.. insted of this we use this method ...
+
+const {courseinstructor : insturctor} = course
+//console.log(courseinstructor);
+console.log(insturctor);
+
+ 
