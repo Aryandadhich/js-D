2d465edbfefa58410003f6212a40f9fd64ce8ed3
aryan dadhich <66268067+Aryandadhich@users.noreply.github.com>
2024-05-21 14:06:08 +0000

    scope

--- 03_basics/02_scops.js
+++ 03_basics/02_scops.js
@@ -7,7 +7,30 @@ if (true)
     const b = 20 
     console.log("Inner:",a);
  }
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
+
+function one(){
+    const username = "Aryan"
+
+    function two ()
+    {
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+    two()
+}
+//one()
+
+if (true)
+    {
+        const username = "Aryan"
+        if (username === "Aryan")
+            {
+                const website = "youtube"
+                console.log(username + website);
+            }
+    }
 
