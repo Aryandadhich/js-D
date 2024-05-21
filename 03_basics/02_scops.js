a5527e75c1e0229885393c9fc1d9f585043f55e3
aryan dadhich <66268067+Aryandadhich@users.noreply.github.com>
2024-05-21 13:54:54 +0000

    scope

--- 03_basics/02_scops.js
+++ 03_basics/02_scops.js
@@ -1,9 +1,13 @@
+//var c = 300
+//outside if global scope
+let a = 300
 if (true)
-{   let a = 10 
+{   //inside if local scope
+    let a = 10 
     const b = 20 
-    var c = 30 
+    console.log("Inner:",a);
  }
-//console.log(a);
+console.log(a);
 //console.log(b);
-console.log(c);
+//console.log(c);
 
