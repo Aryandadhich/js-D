73fa5f6a8451394ba7991b4ee7dfd671ec919c0f
aryan dadhich <66268067+Aryandadhich@users.noreply.github.com>
2024-05-22 11:55:42 +0000

    scope

--- 03_basics/02_scops.js
+++ 03_basics/02_scops.js
@@ -5,7 +5,7 @@ if (true)
 {   //inside if local scope
     let a = 10 
     const b = 20 
-    console.log("Inner:",a);
+    //console.log("Inner:",a);
  }
 //console.log(a);
 //console.log(b);
@@ -17,7 +17,7 @@ function one(){
     function two ()
     {
         const website = "youtube"
-        console.log(username);
+        //console.log(username);
     }
     //console.log(website);
     two()
@@ -30,9 +30,27 @@ if (true)
         if (username === "Aryan")
             {
                 const website = "youtube"
-                console.log(username + website);
+                //console.log(username + website);
             }
             //console.log(website);
     }
     //console.log(username);
 
+console.log(addone(5))
+function addone(num)
+{
+    return num + 1
+}
+
+console.log(addone(5))
+//in this addone work on both conditioin above function or after function
+
+//console.log(addtwo(5)); //this will give error
+const addtwo = function(num)
+{
+    return num + 2
+}
+
+console.log(addtwo(5));// this will work 
+
+// in this addtwo work after only the code because we store the value in addtwo variable
