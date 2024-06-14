e1d35adf4e09f8544a3d6aa401f848950202c0a9
aryan dadhich <66268067+Aryandadhich@users.noreply.github.com>
2024-06-14 12:28:16 +0000

    truthy,flasy

---
+++ 04_basics/control_flow.js
@@ -0,0 +1,17 @@
+// // switch case 
+
+// const month = 2
+
+// switch (month)
+// {
+//     case 1:
+//         console.log("january")
+//         break;
+//     case 2:
+//         console.log("feb")
+//         break;
+
+//         default:
+//             console.log("defalut")
+//         break;
+// }
