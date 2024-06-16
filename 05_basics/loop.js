9cf80dba37cf114ea881ebb76dd2d179f6ba359d
aryan dadhich <66268067+Aryandadhich@users.noreply.github.com>
2024-06-16 10:45:57 +0000

    loop

--- 05_basics/loop.js
+++ 05_basics/loop.js
@@ -2,16 +2,22 @@
 
 for (let index = 0; index < 10; index++) {
     const element = index;
-    console.log(element);
+   // console.log(element);
     
 }
 
 for (let i = 0; i < 10; i++) {
-    console.log(`outer loop value : ${i}`);
+    //console.log(`outer loop value : ${i}`);
     for (let j = 0; j < 10; j++) {
         //console.log(`inner loop value : ${j}`);
-        console.log(i + '*' + j + '=' + i*j )
-        
+        //console.log(i + '*' + j + '=' + i*j )
+    }
     }
 
+let myarray = ["flash", "batman" , "superman"]
+console.log(myarray.length)
+    for (let index = 0; index <= myarray.length; index++) {
+        const element = myarray[index];
+        console.log(element);
+        
     }
