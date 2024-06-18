7af409d17e86f854fe6c39b91ca815d62dbf1e82
aryan dadhich <66268067+Aryandadhich@users.noreply.github.com>
2024-06-18 10:40:00 +0000

    loop3

--- 05_basics/Three.js
+++ 05_basics/Three.js
@@ -3,5 +3,11 @@
 const arr = [1,2,3,4,5]
 
 for (const num of arr) {
-    
+    console.log(num);
+}
+
+const greetings = "Hello world"
+
+for (const greet of greetings) {
+    console.log(`each char is ${greet}`)
 }
