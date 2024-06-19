7b968ac4346587aa1938e68067e05156f6f6c85c
aryan dadhich <66268067+Aryandadhich@users.noreply.github.com>
2024-06-19 12:21:25 +0000

    map

--- 05_basics/Three.js
+++ 05_basics/Three.js
@@ -3,17 +3,23 @@
 const arr = [1,2,3,4,5]
 
 for (const num of arr) {
-    console.log(num);
+    //console.log(num);
 }
 
 const greetings = "Hello world"
 
 for (const greet of greetings) {
-    console.log(`each char is ${greet}`)
+    //console.log(`each char is ${greet}`)
 }
 
 //Maps 
 
-const map = new map()
+const map = new Map()
 map.set('IN',"india")
 map.set('IND',"indonasia")
+
+//console.log(map)
+
+for (const [Key , value] of map) {
+    console.log(Key, ':-', value);
+}
