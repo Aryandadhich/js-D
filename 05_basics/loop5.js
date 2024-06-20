008b451c8505ba6ee04cfb72a2d5412344a5728c
aryan dadhich <66268067+Aryandadhich@users.noreply.github.com>
2024-06-20 10:25:54 +0000

    loop5

--- 05_basics/loop5.js
+++ 05_basics/loop5.js
@@ -2,6 +2,39 @@
 
 const coding = ["js","ruby","java","python"]
 
-coding.forEach( function (val) {
-console.log(val);
+// coding.forEach( function (val) {
+// console.log(val);
+// })
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item , index , arr) => {
+// console.log(item , index , arr)
+// })
+
+const myCoding = [
+    {
+    languageName : "javascript",
+    languageFilename : "js",
+    },
+    {
+        languageName : "python",
+        languageFilename : "py",
+    },
+    {
+        languageName : "java",
+        languageFilename : "java",
+    }
+]
+
+myCoding.forEach( (item) => {
+ console.log(item.languageName);
 })
