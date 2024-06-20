8ef1055a74462bb934cb9ddf4dd531998b578f62
aryan dadhich <66268067+Aryandadhich@users.noreply.github.com>
2024-06-20 10:36:38 +0000

    loop6

---
+++ 05_basics/loop6.js
@@ -0,0 +1,13 @@
+const coding = ["js","ruby","java","python"]
+
+// const values = coding.forEach( (item) => {
+//     //console.log(item)
+//     //return item
+// })
+
+// console.log(values);
+
+const mynums = [1,2,3,4,5,6,7,8,9,10]
+
+const newnums = mynums.filter( (num) => num > 4 )
+console.log(newnums);
