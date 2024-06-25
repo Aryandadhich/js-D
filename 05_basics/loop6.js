3c3890de5db132276e2cdeed5cd1e9e522aa32ed
aryan dadhich <66268067+Aryandadhich@users.noreply.github.com>
2024-06-25 11:23:43 +0000

    filter & map & reduce

--- 05_basics/loop6.js
+++ 05_basics/loop6.js
@@ -7,7 +7,16 @@
 
 // console.log(values);
 
-const mynums = [1,2,3,4,5,6,7,8,9,10]
+// const mynums = [1,2,3,4,5,6,7,8,9,10]
 
-const newnums = mynums.filter( (num) => num > 4 )
-console.log(newnums);
+// const newnums = mynums.filter( (num) => num > 4 )
+// console.log(newnums);
+
+const newnums = []
+
+mynums.forEach((num) => {
+    if (num > 4) {
+        newnums.push(num)
+    }
+});
+console.log(nuwnums);
