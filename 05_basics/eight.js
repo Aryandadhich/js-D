ca1666ec2b4bd58b51337d9c06a20e217d5beaee
aryan dadhich <66268067+Aryandadhich@users.noreply.github.com>
2024-06-25 18:40:23 +0000

    reducde

--- 05_basics/eight.js
+++ 05_basics/eight.js
@@ -1,7 +1,16 @@
 const mynums = [1,2,3]
 
-let mytotal = mynums.reduce(function (acc,currval) {
-    console.log(`acc : ${acc} and currval ${currval}`);
-    return acc + currval
-}, 0 )
+
+// in the function
+// let mytotal = mynums.reduce(function (acc,currval) {
+//     console.log(`acc : ${acc} and currval ${currval}`);
+//     return acc + currval
+// }, 0)
+
+// in arrow function 
+const mytotal = mynums.reduce( (acc , currval) => acc+currval , 0)
 console.log(mytotal);
+
+const shopingcart = [
+    
+]
