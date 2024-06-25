98c20d24b043dd82c2a94d98dcdf5376b540cb48
aryan dadhich <66268067+Aryandadhich@users.noreply.github.com>
2024-06-25 18:23:59 +0000

    map

--- 05_basics/sevn.js
+++ 05_basics/sevn.js
@@ -1,4 +1,9 @@
 const mynumbers = [1,2,3,4,5,6,7,8,9,10]
 
-const newnums = mynumbers.map( (num) => num + 10) 
+//const newnums = mynumbers.map( (num) => num + 10) 
+
+const newnums = mynumbers
+            .map( (num) => num *10)
+            .map((num) => num+1 )
+            .filter((num) => num >= 40)
 console.log(newnums);
